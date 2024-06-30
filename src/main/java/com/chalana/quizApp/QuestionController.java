a3311adfeddef3517830606004a1a3882f1e3a9e
chalana kalpitha <chalanakalpitha72@gmail.com>
2024-06-30 13:56:57 +0530

    initial commit

---
+++ src/main/java/com/chalana/quizApp/QuestionController.java
@@ -0,0 +1,9 @@
+package com.chalana.quizApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+}
